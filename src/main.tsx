58b851e6e30ebb7f7d6d5d75f0d08ecaaa4a5292
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-06-28 19:21:51 -0300

    feat(mirage): adiciona mirage para simular chamado a api

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { globalStyles } from './styles/global.ts';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 globalStyles();
 
